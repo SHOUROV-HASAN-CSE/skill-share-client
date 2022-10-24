72cab615d05603af4d8b573191ba8f2e07aa9478
shourov hasan <shourovhasan172@gmail.com>
2022-10-25 00:40:19 +0600

    install and import firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAR3hpgZv1jcTN77P1JHhV_2gCsygMm3yQ",
+  authDomain: "shillshare.firebaseapp.com",
+  projectId: "shillshare",
+  storageBucket: "shillshare.appspot.com",
+  messagingSenderId: "830607649962",
+  appId: "1:830607649962:web:d9c668983040f8ee1d34d3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
