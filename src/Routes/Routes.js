2f77541896165480a54dd337e86dcdc9f68dab51
shourov hasan <shourovhasan172@gmail.com>
2022-10-25 11:25:34 +0600

    error component and error route created

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,4 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
+import Category from "../component/Category/Category";
+import Courses from "../component/Courses/Courses";
+import Error from "../component/Error/Error";
 import Home from "../component/Home/Home";
 import Main from "../layout/Main";
 
@@ -12,7 +15,20 @@ export const routes = createBrowserRouter([
       {
         path:'/',
        element:<Home/>
-      }
+      },
+      {
+        path:'/course',
+        element:<Courses/>
+      },
+      {
+        path:'/category/:id',
+        element:<Category/>
+      },
+   
     ]
+  },
+  {
+    path:'*',
+    element:<Error/>
   }
 ]);
