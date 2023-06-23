21918cd8809c92c6673a35c28a1ead134e51f005
Shourov Hasan <shourovhasancse@gmail.com>
2023-06-23 23:18:40 +0600

    banner section almost ready

---
+++ src/component/Home/Banner/Banner.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './Banner.css';
+import { GiBookmarklet } from 'react-icons/gi';
+import { FaGraduationCap} from 'react-icons/fa';
+import { PiCertificateFill} from 'react-icons/pi';
+
+const Banner = () => {
+  return (
+    <div className='banner-image'>
+      <Container>
+        <p>.</p>
+        <h1 className='bannerhead'>Learn high demanding <span className='feshead2'>Skills from Experts</span> and start making money <br /> online!</h1>
+        <p className='bannerp'>Earn money from anywhere by leveraging your newly learned skills. LWS Academy will teach you the necessary skills to become a freelancer by explaining how to find clients, negotiate rates, market yourself as a freelancer, etc. on the go!</p>
+
+        <div>
+        <button className='mt-4 cb1'> All Courses</button>
+        <button className='mt-4 cb2'> Instructors</button>
+        </div>
+
+        <div className='go'>
+        <Row >
+        <Col  lg="4">
+        <div className='bannerhbtn'>
+        <GiBookmarklet className='bannerlogo'/>
+        <div>
+          <h2 className='bannerh'>Courses</h2>
+          <p>view Courses</p>
+        </div>
+        </div>
+        </Col>
+
+
+        <Col  lg="4">
+        <div className='bannerhbtn'>
+        <PiCertificateFill className='bannerlogo'/>
+        <div>
+          <h2 className='bannerh'>Courses</h2>
+          <p>view All</p>
+        </div>
+        </div>
+        </Col>
+
+
+        <Col  lg="4">
+        <div className='bannerhbtn'>
+        <FaGraduationCap className='bannerlogo'/>
+        <div>
+          <h2 className='bannerh'>Courses</h2>
+          <p>view All</p>
+        </div>
+        </div>
+        </Col>
+        </Row>
+        </div>
+
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import Banner from './Banner/Banner';
 import Features from './Features/Features';
 import Meeting from './Meeting/Meeting';
 import Review from './Review/Review';
@@ -11,14 +12,15 @@ import Testimonial from './Testimonial/Testimonial';
 const Home = () => {
   return (
     <div>
-       <Container>
+      <Banner/>
+       {/* <Container>
        <Card className="text-center">
       <Card.Body>
       <Card.Img variant="top" src='https://codetoinvent.com/wp-content/uploads/2020/12/programming-courses-back.jpg' />
         <Link to={'/course'}><Button variant="info" className='mt-5 fs-4 text-white'>All Course</Button></Link>
       </Card.Body>
     </Card>
-       </Container>
+       </Container> */}
        <Features/>
        <Review/>
        <Meeting/>
